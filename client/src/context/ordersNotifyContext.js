82d1cff99dec022c81984e6d9851b0ab48be76dd
Ansar Mehmood <ansarkhanmehmood@gmail.com>
2026-08-13 15:56:01 +0500

    Fix React build warnings

--- client/src/context/ordersNotifyContext.js
+++ client/src/context/ordersNotifyContext.js
@@ -1,16 +1,20 @@
-import React, { createContext, useState, useEffect,useContext } from 'react';
-import axios from 'axios';
-import {useAuth} from './auth'; // Adjust the import path as necessary
+import React, { createContext, useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { useAuth } from "./auth"; // Adjust the import path as necessary
 
 const OrdersContext = createContext();
 
 const OrdersProvider = ({ children }) => {
   const [orders, setOrders] = useState([]);
-  const [auth,setAuth] = useAuth();
+  const [auth, setAuth] = useAuth();
 
   const getOrders = async () => {
     try {
-      const { data } = await axios.get('/api/v1/auth/all-orders');
+      const { data } = await axios.get("/api/v1/auth/all-orders", {
+        headers: {
+          Authorization: `Bearer ${auth?.token}`,
+        },
+      });
       setOrders(data);
     } catch (error) {
       console.log(error);
@@ -24,11 +28,11 @@ const OrdersProvider = ({ children }) => {
   }, [auth?.token]);
 
   return (
-    <OrdersContext.Provider value={[ orders, setOrders, getOrders ]}>
+    <OrdersContext.Provider value={[orders, setOrders, getOrders]}>
       {children}
     </OrdersContext.Provider>
   );
 };
 
 const useOrders = () => useContext(OrdersContext);
-export { useOrders,  OrdersProvider };
+export { useOrders, OrdersProvider };
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,8 +7,9 @@ export const requireSignIn = async (req, resp, next) => {
   //"next" parameter is executed till we will be hanged in previous code so it is good for
   //security
   try {
+    const token = req.headers.authorization?.split(" ")[1];
     const decode = JWT.verify(
-      req.headers.authorization,
+      token,
       process.env.JWT_SECRET
     );
     //as token are placed inside headers in authorization so we use req.headers not req.body
